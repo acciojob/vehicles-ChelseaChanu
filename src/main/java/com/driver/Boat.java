01614656365f90dc586b27f0db18d6b14c3391dd
Chelsea <chelseach1asnu@gmail.com>
2023-01-21 16:05:40 +0530

    Vehicle Assignment

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,11 +4,6 @@ public class Boat implements WaterVehicle {
     String name;
     int capacity;
 
-    public Boat(String name, int capacity){
-        this.name = name;
-        this.capacity = capacity;
-    }
-
     @Override
     public String getVehicleName() {
         return this.name;
